e9007cb235aafc07e92634fc42f040b1417333f8
Haneen Hashlamoun <haneen.hashlamoun@gmail.com>
2021-10-12 23:15:14 +0300

    lab 04

---
+++ madlib_cli/madlib_cli.py
@@ -0,0 +1,8 @@
+
+
+def read_template(pathOfFile):
+    try :
+        with open(pathOfFile)as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError("file not found")
